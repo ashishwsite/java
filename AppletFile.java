59581494f12ee2736f7bbdc6144974f1cc8e5ac2
ashish <1508ramashish@gmail.com>
2024-02-11 12:52:25 +0530

    appletmodi

--- AppletFile.java
+++
@@ -1,7 +0,0 @@
-import java.applet.Applet;
-import java.awt.Graphics;
-public class AppletFile  extends Applet{
-    public void paint(Graphics g){
-        g.drawString("hello world", 150, 150);
-    }
-}
---
+++ applet/AppletFile.java
@@ -0,0 +1,14 @@
+//  link:https://www.javatpoint.com/java-applet
+//  applet is ui made using java
+//  it has many predefine method 
+//  applet is lokking like window 
+//  applet has 5 impoertant method
+//  applet is run using coomand appletviwer 
+//  applet file required html file to run 
+import java.applet.Applet;
+import java.awt.Graphics;
+public class AppletFile  extends Applet{
+    public void paint(Graphics g){
+        g.drawString("hello world", 150, 150);
+    }
+}
